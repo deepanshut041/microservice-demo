4c501da03057758e6ad46b70d0b38d390ad835a1
deepanshut041 <deepanshut041@gmail.com>
2019-01-10 22:31:18 +0530

    Initial commit with config and reservation-service

---
+++ reservation-service/src/main/java/io/github/deepanshut041/reservationservice/ReservationServiceApplication.java
@@ -0,0 +1,14 @@
+package io.github.deepanshut041.reservationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationServiceApplication.class, args);
+	}
+
+}
+
