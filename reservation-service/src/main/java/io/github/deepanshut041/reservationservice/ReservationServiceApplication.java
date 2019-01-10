8ba69a09be44bac5d3e91ece9edf1c4e9ce4c8c8
deepanshut041 <deepanshut041@gmail.com>
2019-01-11 00:27:35 +0530

    Configured eureka service with reservation service

--- reservation-service/src/main/java/io/github/deepanshut041/reservationservice/ReservationServiceApplication.java
+++ reservation-service/src/main/java/io/github/deepanshut041/reservationservice/ReservationServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -19,6 +20,7 @@ import javax.persistence.Id;
 import java.util.Collection;
 import java.util.stream.Stream;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ReservationServiceApplication {
 
